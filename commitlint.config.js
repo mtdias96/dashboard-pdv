d202a16b2a7b86e0561861f4400462e8b5fb7905
mtdias96 <matheusdias.front96@gmail.com>
2025-01-05 22:04:56 -0300

    feat: add shadcn

--- commitlint.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  parserPreset: {
-    parserOpts: {
-      headerPattern: /^(feat|fix|docs|style|refactor|test|chore): (\w*)\/(.*)$/,
-      headerCorrespondence: ['type', 'scope', 'subject'],
-    },
-  },
-};
