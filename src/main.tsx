c6e660a1eb381373188291ebc6f3fe7db45c39eb
mtdias96 <matheusdias.front96@gmail.com>
2025-01-08 10:06:40 -0300

    feat: main/add browser router

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import './styles/index.css';
+import '@/styles/index.css';
 
+import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </BrowserRouter>,
 );
