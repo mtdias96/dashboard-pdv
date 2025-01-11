db260b107d78a68c465d088467cc2c46e1e0c09b
mtdias96 <matheusdias.front96@gmail.com>
2025-01-11 10:01:10 -0300

    feat: main/add auth provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client';
 
 import '@/styles/index.css';
 
+import { AuthProvider } from '@/app/contexts/AuthContext';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>,
+  <AuthProvider>
+    <BrowserRouter>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </BrowserRouter>
+  </AuthProvider>,
 );
