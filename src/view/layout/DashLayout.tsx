35ba8272d41613eedb7bbeebc5d5331c6a55877e
mtdias96 <matheusdias.front96@gmail.com>
2025-01-13 14:17:58 -0300

    feat: DashLayout/add layout dashboard

--- src/view/layout/DashLayout.tsx
+++ src/view/layout/DashLayout.tsx
@@ -1,9 +1,5 @@
 import { AppSidebar } from '@/view/components/dashboard/AppSidebar';
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from '@/view/components/ui/sidebar';
+import { SidebarInset, SidebarProvider } from '@/view/components/ui/sidebar';
 import { Outlet } from 'react-router-dom';
 
 export default function DashLayout() {
@@ -11,14 +7,7 @@ export default function DashLayout() {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-          </div>
-        </header>
-
-        {/*Adicionar component de skeleton*/}
-        <div className="px-4">
+        <div className="pl-9 pr-20 mt-11">
           <Outlet />
         </div>
       </SidebarInset>
