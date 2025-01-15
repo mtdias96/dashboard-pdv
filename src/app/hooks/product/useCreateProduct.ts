237e6e047dfe24cdf33a7c30fbc7133c136fdf6f
mtdias96 <matheusdias.front96@gmail.com>
2025-01-15 16:26:16 -0300

    feat: useCreateProduct/add refetch and new layout toast

--- src/app/hooks/product/useCreateProduct.ts
+++ src/app/hooks/product/useCreateProduct.ts
@@ -1,9 +1,11 @@
+import { useGetProducts } from '@/app/hooks/product/useGetProduct';
 import { IProduct } from '@/app/interfaces/IProduct';
 import { ProductService } from '@/app/services/ProductService';
 import { useMutation } from '@tanstack/react-query';
 import { toast } from 'sonner';
 
 export function useCreateProduct() {
+  const { refetch } = useGetProducts();
   const { mutateAsync, isPending } = useMutation({
     mutationFn: async (product: IProduct): Promise<void> => {
       await ProductService.createProduct(product);
@@ -11,8 +13,10 @@ export function useCreateProduct() {
     onSuccess: () => {
       toast.success('Produto criado com sucesso!', {
         icon: '✅',
-        className: 'text-lg font-bold text-green-600',
+        className:
+          'text-sm text-gray-800 bg-white border border-gray-300 p-3 rounded-lg shadow-md transition-all duration-300 ease-in-out transform hover:scale-95',
       });
+      refetch();
     },
   });
 
