d2a3a8178ae31722cf8f15c4eb7b27523e23ad61
mtdias96 <matheusdias.front96@gmail.com>
2025-01-16 15:35:14 -0300

    feat: IProduct/add opcional type

--- src/app/interfaces/IProduct.ts
+++ src/app/interfaces/IProduct.ts
@@ -2,8 +2,8 @@ export interface IProduct {
   id: string;
   name: string;
   price: number;
-  imageUrl: string;
-  image: File;
+  imageUrl?: string;
+  image?: File;
   categoryId: string;
   stock: number;
   description?: string;
